bcc6df29db1be8f6535611940f0fd0d17bcee857
yeiny gonzalez cifuentes <yeinygonzalezcifuentes@gmail.com>
2016-09-14 12:32:29 -0500

    ejercicio terminado

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
+document.getElementsByTagName("h2")[0].innerHTML = "Por favor inicia sesión";
 document.getElementsByName("email")[0].placeholder="Correo electrónico";
 document.getElementsByName("password")[0].placeholder="Contraseña";
+document.getElementsByTagName("span")[0].innerHTML = "Recordar datos";
+document.getElementsByTagName("button")[0].innerHTML = "Iniciar sesión";
