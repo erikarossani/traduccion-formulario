1dd1ff2f5ed45aaade552c3cd55b6641d0c1f007
yeiny gonzalez cifuentes <yeinygonzalezcifuentes@gmail.com>
2016-09-14 13:34:45 -0500

    modificacion del ejercicio final

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 document.getElementsByTagName("h2")[0].innerHTML = "Por favor inicia sesión";
-document.getElementsByName("email")[0].placeholder="Correo electrónico";
-document.getElementsByName("password")[0].placeholder="Contraseña";
+document.getElementById("inputEmail").placeholder="Correo electrónico";
+document.getElementById("inputPassword").placeholder="Contraseña";
 document.getElementsByTagName("span")[0].innerHTML = "Recordar datos";
 document.getElementsByTagName("button")[0].innerHTML = "Iniciar sesión";
